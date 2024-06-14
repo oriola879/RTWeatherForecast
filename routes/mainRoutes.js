334384c75d7e26a166612b7414b36151419223d8
Oriola <oriola.arifi24@gmail.com>
2024-06-14 19:13:34 +0200

    Updating password finished and added the logic to secure the routes

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -3,34 +3,18 @@ const express = require('express');
 const authController = require('../controllers/authController')
 const mainController = require('../controllers/mainController')
 const accountController = require('../controllers/accountController');
+const { ensureAuth } = require('../middleware/auth')
 const router = express.Router();
 
 router.get('/', mainController.getIndex)
-router.get('/account', mainController.getAccount)
-router.post('/account/update-username', accountController.updateUsername);
-router.post('/account/update-email', accountController.updateEmail);
-router.post('/account/update-password', accountController.updatePassword);
+router.get('/account',ensureAuth, mainController.getAccount)
+router.post('/account/update-username',ensureAuth, accountController.updateUsername);
+router.post('/account/update-email',ensureAuth, accountController.updateEmail);
+router.post('/account/update-password',ensureAuth,accountController.updatePassword);
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 router.get('/signup', authController.getSignup);
 router.post('/signup', authController.postSignup);
-router.get('/logout', authController.logout);
-
-// router.get('/projects', mainController.getProjects)
-// router.get('/posts', mainController.getPosts)
-
-//language switching 
-// router.get('/index', (req, res) => {
-//     const lang = req.query.clang;
-//     if(lang === 'al'|| lang === "en"){
-//         res.redirect("/");
-//     }
-// });
-
-// //This was made to make the link that appears on google to redirect to our page
-// router.get('/en', (req, res) => {
-//         res.redirect("/");
-// });
-
+router.get('/logout',ensureAuth, authController.logout);
 
 module.exports = router
